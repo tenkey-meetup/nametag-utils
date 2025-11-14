df1ab7dc80f0ae93017f6a32ca4d84c2e3acf7b1
ai03 <admin@ai03.com>
2025-11-14 19:16:32 +0900

    Minimum cleanup for a staff tool

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,13 @@ export const Route = createFileRoute('/')({
 function IndexComponent() {
   return (
     <div class="text-center">
-      <header class="min-h-screen flex flex-col items-center justify-center bg-gray-800 text-white text-[calc(10px+2vmin)]">
-        <div class="flex flex-col items-center gap-12">
+        <div class="flex flex-col items-center gap-12 bg-gray-800 w-full h-[calc(100dvh-65px)] flex-grow grow align-center justify-center text-white">
           <TenkeyLogo classes='w-[75dvw] md:w-[50dvw] lg:w-[25dvw]' />
+          <p class="text-4xl">スタッフ用ツール</p>
           <div class="flex flex-row gap-4">
             <Link class="btn btn-primary" to={"/nametag-checker"}>名札の存在確認</Link>
           </div>
         </div>
-      </header>
     </div>
   )
 }
